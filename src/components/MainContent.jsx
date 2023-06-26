7f2fca9720ca6869537a3a533680b7d7f1096423
Jpsoaresfig <jpbusinesss@hotmail.com>
2023-06-26 14:22:49 -0300

    correcao de caminho

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -2,7 +2,7 @@ import TechnologiesContainer from "./TechnologiesContainer";
 import AboutContainer from "./AboutContainer";
 import ProjectsContainer from "./ProjectsContainers";
 
-import "../styles/components/maincontent.sass";
+import "../styles/components/maincontents.sass";
 
 const MainContent = () => {
   return (
