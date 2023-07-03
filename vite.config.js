0c35f2280d2c0f538065654dcb958afa541dcdad
Jpsoaresfig <jpbusinesss@hotmail.com>
2023-07-03 13:52:25 -0300

    ajuste vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import sassPlugin from 'vite-plugin-sass';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sassPlugin()],
-});
+  plugins: [react()],
+})
