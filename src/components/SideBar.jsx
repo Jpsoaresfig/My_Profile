1c63aa076e5188730f5277d07cf3724abfed2c0f
Jpsoaresfig <jpbusinesss@hotmail.com>
2023-07-05 16:41:08 -0300

    ajustando texto e colocando video

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -22,19 +22,15 @@ const Sidebar = () => {
 
 
       <h2 id="AboutMe">Sobre mim:</h2>
-      <p> Desde pequeno, meu interesse pela tecnologia foi crescendo e despertando aos poucos: eu usava o computador do meu pai e fingia programar, apertando botões aleatoriamente.</p>
-       <p>Hoje, aos meus 21 anos, estou transformando essa paixão em realidade. Participei e ainda participo de projetos na minha área, sempre buscando meu desenvolvimento pessoal e crescimento tanto como indivíduo quanto como programador.</p>
+      <p>Sendo um nativo digital, desde a infância já nutria interesse por esse tema. Cheguei a criar um canal no YouTube com meu pai, que é publicitário, chamado JPTec. </p>
+      <p> Acredito que essa experiência tenha me influenciado significativamente na escolha da minha graduação.</p>
+      <p>Ao estudar Ciência da Computação, percebo que estou trilhando o caminho certo. Desfruto do que estudo e faço, o que me proporciona um crescimento contínuo.</p>
+      <p> Estou determinado a expandir meus conhecimentos e fazer parte das inovações, maximizando minha experiência por meio de oportunidades de aprendizado de alto impacto.</p>
 
-        <p>Desfruto imensamente do que faço e estou sempre em busca de atualização como programador, para me aprimorar cada vez mais nesse vasto campo que é a Ciência da Computação. </p>
+    <h2 id="AboutMe">Meu primeiro Job: </h2>
+      <iframe id="videojptec" width="500" height="315" src="https://www.youtube.com/embed/tZflWQKvyJw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
-        <p>Através dos meus estudos em Ciências da Computação, estou adquirindo uma base sólida em conceitos fundamentais, como algoritmos e estruturas de dados. </p>
 
-        <p>Acredito que a Ciência da Computação é um campo que permite um crescimento contínuo, tanto pessoal quanto profissionalmente. Estou determinado a expandir meus conhecimentos e ser parte das inovações.</p>
-
-       
-      
-
-      
     </aside>
   );
 };
