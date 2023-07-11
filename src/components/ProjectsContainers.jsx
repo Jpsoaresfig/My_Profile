bd0abe3b2552ea008740e5228aeb9b0b73bb10d2
Jpsoaresfig <jpbusinesss@hotmail.com>
2023-07-11 13:25:47 -0300

    ajustando texto

--- src/components/ProjectsContainers.jsx
+++ src/components/ProjectsContainers.jsx
@@ -7,7 +7,7 @@ const ProjectsContainer = () => {
       <h2>Projetos: </h2>
       <p id="aboutproject">
         Sou um aluno dedicado cursando Ciência da Computação, com uma paixão pelo desenvolvimento de projetos. Ao longo da minha jornada acadêmica, tive a oportunidade de trabalhar em diversos projetos tanto dentro quanto fora da faculdade.
-        Gostaria de convidá-lo(a) a dar uma olhada em alguns dos meus projetos. Clique no link abaixo para explorar o meu portfólio:
+        Gostaria de convidá-lo(a) a dar uma olhada em alguns dos meus projetos. Clique no botao abaixo para explorar o meu portfólio:
       </p>
       <br />
       <a href="https://github.com/Jpsoaresfig?tab=repositories" className="bton">
