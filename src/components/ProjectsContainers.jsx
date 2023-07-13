c1a9e5acb3387d12a2e44daf7b3ac4988518637d
Jpsoaresfig <jpbusinesss@hotmail.com>
2023-07-13 13:02:16 -0300

    ajuste da parte de projetos

--- src/components/ProjectsContainers.jsx
+++ src/components/ProjectsContainers.jsx
@@ -1,13 +1,13 @@
-
+import projet1 from "../img/discordpage.png";
+import projet2 from "../img/photogallery.png";
+import projet3 from "../img/todolist.png";
 
 import '../Styles/components/projectcontainer.sass';
 
 const ProjectsContainer = () => {
-
   return (
     <>
       <h2>Projetos</h2>
-
       <p id="aboutproject">
         Anexo, encontra-se meu portfólio, que reúne uma seleção dos projetos acadêmicos e autorais que desenvolvi até o momento.
       </p>
@@ -19,21 +19,19 @@ const ProjectsContainer = () => {
       <br />
       <br />
       <h3 className='titup1'>Página Discord</h3>
-      <img src="src\img\discordpage.png" className='project1' alt="" />
+      <img src={projet1} className='project1' alt="" />
       <p className='texto1'>Uma página discord feita para treinar HTML e CSS</p>
       <br />
       <h3 className='titup2'>PhotoGallery</h3>
-      <img src="src\img\photogallery.png" className='project2' alt="" />
-      <p className='texto2'>Uma página web que consome uma API e exibe as fotos usando react</p>
+      <img src={projet2} className='project2' alt="" />
+      <p className='texto2'>Uma página web que consome uma API e exibe as fotos usando React</p>
       <br />
       <h3 className='titup3'>To do List</h3>
-      <img src="src\img\todolist.png" className='project3' alt="" />
-      <p className='texto3'>Uma to do list para treinar HTML, CSS e JAVASCRIPT</p>
+      <img src={projet3} className='project3' alt="" />
+      <p className='texto3'>Uma to-do list para treinar HTML, CSS e JavaScript</p>
       <br />
-
       <center><h2>Fim ;)</h2></center>
     </>
-
   );
 };
 
